5b7c845dd8c794a7298ee36369adb6b5eaddb88e
ahmed_shabasy <ahmed.shabasy16@gmail.com>
2023-10-14 02:46:17 -0700

    nes

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isupper - recognize if character is upper case
+ *
+ * @c: function input
+ *
+ * Return: 1 (success), otherwise 0
+*/
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 95)
+		return (1);
+	else
+		return (0);
+}
