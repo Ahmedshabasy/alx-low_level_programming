82de89ce27336475006ab6dad8e0d432d5c46d21
ahmed_shabasy <ahmed.shabasy16@gmail.com>
2023-10-15 15:59:23 -0700

    nes

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -10,12 +10,14 @@ void print_line(int n)
 {
 	int i;
 
-	if (n <= 0){
+	if (n <= 0)
+	{
 		_putchar('\n');
 		return;
 	}
-		for (i = 1; i <= n; i++){
+		for (i = 1; i <= n; i++)
+		{
 			_putchar('_');
-	}
+		}
 		_putchar('\n');
 }
