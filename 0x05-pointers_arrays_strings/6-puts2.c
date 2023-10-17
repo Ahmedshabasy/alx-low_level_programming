74f5717bfaff21ff03629ae3e54046fb62f96ad3
ahmed_shabasy <ahmed.shabasy16@gmail.com>
2023-10-16 19:01:53 -0700

    pointer

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -12,10 +12,10 @@ void puts2(char *str)
 {
 	int i;
 
-	for (int i = 0;str[i] !='\0'; ++i)
+	for (i = 0;str[i] !='\0'; ++i)
 	{
 		if (i % 2 == 0)
-		_putchar(str[i]);
+			_putchar(str[i]);
 	}
 	_putchar('\n');
 }
