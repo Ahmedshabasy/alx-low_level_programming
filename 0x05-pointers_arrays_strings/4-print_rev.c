68784b1427039ed8129345e80f8093e3648f67b4
ahmed_shabasy <ahmed.shabasy16@gmail.com>
2023-10-16 18:31:32 -0700

    pointer

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,9 +9,10 @@
 void print_rev(char *s)
 {
 	int len = strlen(s);
+	int i;
+	
+	for (i = len - 1; i >= 0; i--)
+		putchar(s[i]);
 
-	for (int i = len - 1; i >= 0; i--)
-		_putchar(s[i]);
-
-	_putchar('\n');
+	putchar('\n');
 }
