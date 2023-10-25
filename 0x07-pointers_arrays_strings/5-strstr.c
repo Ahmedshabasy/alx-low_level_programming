cda8057dd5fe942d8092af7e7d12fc866e5ae973
ahmed_shabasy <ahmed.shabasy16@gmail.com>
2023-10-24 17:32:35 -0700

    even

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -11,24 +11,25 @@
 
 char *_strstr(char *haystack, char *needle)
 {
-	if (*needle == '\0')
+
+	char h = haystack;
+	char n = needle;
+
+	if (needle == '\0')
 	{
 		return (haystack);
 	}
 
-	while (*haystack != '\0')
+	while (haystack != '\0')
 	{
-		if (*haystack == *needle)
+		if (haystack == needle)
 		{
-			char *h = haystack;
-			char *n = needle;
-
-			while (*n != '\0' && *h == *n)
+			while (n != '\0' && h == n)
 			{
 				h++;
 				n++;
 			}
-			if (*n == '\0')
+			if (n == '\0')
 			{
 				return (haystack);
 			}
