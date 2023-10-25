4acdf7783cbc8782628830c2006a9e6344ddf2e3
ahmed_shabasy <ahmed.shabasy16@gmail.com>
2023-10-24 17:48:47 -0700

    ev

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -6,21 +6,21 @@
  * @haystack: first string input
  * @needle: second string input
  *
- * Return: nothing
+ * Return: 0 (success)
  */
 
 char *_strstr(char *haystack, char *needle)
 {
 	for (; *haystack != '\0'; haystack++)
 	{
-	char *h = haystack;
-	char *n = needle;
+		char *h = haystack;
+		char *n = needle;
 
-	while (*h == *n && *n != '\0')
-	{
-		h++;
-		n++;
-	}
+		while (*h == *n && *n != '\0')
+		{
+			h++;
+			n++;
+		}
 		if (*n == '\0')
 		{
 			return (haystack);
