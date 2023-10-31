5029cb1e7efb5d82d9917d5f319e1411f23336ef
ahmed_shabasy <ahmed.shabasy16@gmail.com>
2023-10-30 19:00:25 -0700

    arg

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -15,7 +15,7 @@ int main(int argc, char *argv[])
 	if (arg == 2)
 	{
 	int i, l = 0, m = atoi(argv[1]);
-	int cents[25, 10, 5, 2, 1];
+	int cents[] = {25, 10, 5, 2, 1};
 
 	for (i = 0; i < 5; i++)
 	{
