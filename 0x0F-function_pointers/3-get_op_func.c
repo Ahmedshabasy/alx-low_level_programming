ff8fab58831d18ad46f59eb9dfffbf54645829e3
ahmedshabasy <you@example.com>
2024-07-27 07:03:18 -0700

    edit

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -10,18 +10,18 @@
 int (*get_op_func(char *s))(int, int)
 {
 	int i = 0;
-       op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
-    };
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
 
 
-    while (ops[i].op != NULL && *(ops[i].op) != *s)
-        i++;
+	while (ops[i].op != NULL && *(ops[i].op) != *s)
+		i++;
 
-    return (ops[i].op == NULL ? NULL : ops[i].f);
+	return (ops[i].op == NULL ? NULL : ops[i].f);
 }
