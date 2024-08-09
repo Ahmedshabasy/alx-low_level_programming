7d761d73942d6d02750e7ede3c752bb3b1a8d337
Ahmed <ahmed.shabasy16@gmail.com>
2024-08-09 16:40:56 -0700

    edit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,8 +8,7 @@
 unsigned int binary_to_uint(const char *b)
 {
 	int a;
-	unsigned int num;
-	num = 0;
+	unsigned int num = 0;
 
 	if (!b)
 		return (0);
@@ -17,7 +16,7 @@ unsigned int binary_to_uint(const char *b)
 	for (a = 0; b[a] != '\0'; a++)
 	{
 		if (b[a] != '0' && b[a] != '1')
-				return (0);
+			return (0);
 	}
 	for (a = 0; b[a] != '\0'; a++)
 	{
