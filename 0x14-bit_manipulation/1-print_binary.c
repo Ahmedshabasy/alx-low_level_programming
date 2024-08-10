1e0b9b13eeca26cb2002b6a10b2fc96ad8ae19e7
Ahmed <ahmed.shabasy16@gmail.com>
2024-08-09 17:22:56 -0700

    edit

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -15,7 +15,6 @@ unsigned long int _pow(unsigned int base, unsigned int power)
 
 	for (a = 1; a <= power; a++)
 		num *= base;
-	
 	return (num);
 }
 
@@ -36,7 +35,6 @@ void print_binary(unsigned long int n)
 	while (divisor != 0)
 	{
 		check = n & divisor;
-
 		if (check == divisor)
 		{
 			flag = 1;
